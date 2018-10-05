d7167383d5ef760d53e85b51be673b9500ef7187
Joseph Ganning <Joseph.ganning@gmail.com>
2018-10-05 01:49:26 -0400

    Update details.js

--- details.js
+++ details.js
@@ -1,3 +1,8 @@
+var siteWidth = 1280;
+var scale = screen.width /siteWidth
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
 $(document).ready(function() {
 	
 	$('.eHideDetails').hide();
